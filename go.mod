5dc2c8091d0500bc278338e2886e16c0e167a889
Daniel Whatmuff <danielwhatmuff@gmail.com>
2019-11-28 15:59:08 +0000

    updating generated code

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/go-openapi/spec v0.19.0
 	github.com/operator-framework/operator-sdk v0.12.0
 	github.com/spf13/pflag v1.0.3
+	golang.org/x/net v0.0.0-20190812203447-cdfb69ac37fc
 	k8s.io/api v0.0.0
 	k8s.io/apimachinery v0.0.0
 	k8s.io/client-go v11.0.0+incompatible
--- pkg/apis/gcp/v1alpha1/zz_generated.openapi.go
+++ pkg/apis/gcp/v1alpha1/zz_generated.openapi.go
@@ -11,9 +11,78 @@ import (
 
 func GetOpenAPIDefinitions(ref common.ReferenceCallback) map[string]common.OpenAPIDefinition {
 	return map[string]common.OpenAPIDefinition{
-		"github.com/appvia/gcp-operator/pkg/apis/gcp/v1alpha1.GCPProject":       schema_pkg_apis_gcp_v1alpha1_GCPProject(ref),
-		"github.com/appvia/gcp-operator/pkg/apis/gcp/v1alpha1.GCPProjectSpec":   schema_pkg_apis_gcp_v1alpha1_GCPProjectSpec(ref),
-		"github.com/appvia/gcp-operator/pkg/apis/gcp/v1alpha1.GCPProjectStatus": schema_pkg_apis_gcp_v1alpha1_GCPProjectStatus(ref),
+		"github.com/appvia/gcp-operator/pkg/apis/gcp/v1alpha1.GCPCredentials":       schema_pkg_apis_gcp_v1alpha1_GCPCredentials(ref),
+		"github.com/appvia/gcp-operator/pkg/apis/gcp/v1alpha1.GCPCredentialsSpec":   schema_pkg_apis_gcp_v1alpha1_GCPCredentialsSpec(ref),
+		"github.com/appvia/gcp-operator/pkg/apis/gcp/v1alpha1.GCPCredentialsStatus": schema_pkg_apis_gcp_v1alpha1_GCPCredentialsStatus(ref),
+		"github.com/appvia/gcp-operator/pkg/apis/gcp/v1alpha1.GCPProject":           schema_pkg_apis_gcp_v1alpha1_GCPProject(ref),
+		"github.com/appvia/gcp-operator/pkg/apis/gcp/v1alpha1.GCPProjectSpec":       schema_pkg_apis_gcp_v1alpha1_GCPProjectSpec(ref),
+		"github.com/appvia/gcp-operator/pkg/apis/gcp/v1alpha1.GCPProjectStatus":     schema_pkg_apis_gcp_v1alpha1_GCPProjectStatus(ref),
+	}
+}
+
+func schema_pkg_apis_gcp_v1alpha1_GCPCredentials(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "GCPCredentials is the Schema for the gcpcredentials API",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"kind": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/api-conventions.md#types-kinds",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"apiVersion": {
+						SchemaProps: spec.SchemaProps{
+							Description: "APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/api-conventions.md#resources",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"metadata": {
+						SchemaProps: spec.SchemaProps{
+							Ref: ref("k8s.io/apimachinery/pkg/apis/meta/v1.ObjectMeta"),
+						},
+					},
+					"spec": {
+						SchemaProps: spec.SchemaProps{
+							Ref: ref("github.com/appvia/gcp-operator/pkg/apis/gcp/v1alpha1.GCPCredentialsSpec"),
+						},
+					},
+					"status": {
+						SchemaProps: spec.SchemaProps{
+							Ref: ref("github.com/appvia/gcp-operator/pkg/apis/gcp/v1alpha1.GCPCredentialsStatus"),
+						},
+					},
+				},
+			},
+		},
+		Dependencies: []string{
+			"github.com/appvia/gcp-operator/pkg/apis/gcp/v1alpha1.GCPCredentialsSpec", "github.com/appvia/gcp-operator/pkg/apis/gcp/v1alpha1.GCPCredentialsStatus", "k8s.io/apimachinery/pkg/apis/meta/v1.ObjectMeta"},
+	}
+}
+
+func schema_pkg_apis_gcp_v1alpha1_GCPCredentialsSpec(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "GCPCredentialsSpec defines the desired state of GCPCredentials",
+				Type:        []string{"object"},
+			},
+		},
+	}
+}
+
+func schema_pkg_apis_gcp_v1alpha1_GCPCredentialsStatus(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "GCPCredentialsStatus defines the observed state of GCPCredentials",
+				Type:        []string{"object"},
+			},
+		},
 	}
 }
 
